86a909f120bade6c223abcdd35eb652075db1e0c
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-04-02 15:28:04 +0600

    Remove uncecessary not found page declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { QuizResultComponent } from './quiz-result/quiz-result.component';
-import { PageNotFoundComponent } from './shared/pages/page-not-found/page-not-found.component';
 import { TopicComponent } from './topic/topic.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuizComponent,
     QuizResultComponent,
-    PageNotFoundComponent,
     TopicComponent
   ],
   imports: [
+    // SharedModule,
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
